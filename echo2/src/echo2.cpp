f61e38c26a536fd8a0f406953881b2f36caaf4bc
Brian K Preston <brian8181@gmail.com>
2022-07-09 11:05:00 -0500

    add echo2 project

---
+++ echo2/src/echo2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <regex>
+#include <getopt.h>
+#include "main.hpp"
+#include "echo2.hpp"
+
+using namespace std;
+
+int parse_options(int argc, char* argv[])
+{
+	//cout << "Hello Template!" << endl;
+	cout << argv[1] << endl;
+
+	return 0;
+}
---
+++ echo2/src/echo2.hpp
@@ -0,0 +1,12 @@
+#ifndef _echo2_HPP
+#define _echo2_HPP
+
+// constants
+
+// Options Flags
+unsigned char DEFAULTS = 0;
+unsigned char option_flags = 0; 
+
+
+#endif
+
