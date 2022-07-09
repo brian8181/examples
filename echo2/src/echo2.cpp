97c0262f55807bf0f2da29c5c6767301f06ce73b
Brian K Preston <brian8181@gmail.com>
2022-07-09 11:17:44 -0500

    add gpl header

--- echo2/src/echo2.cpp
+++ echo2/src/echo2.cpp
@@ -1,3 +1,18 @@
+This file is part of echo2.
+
+echo2 is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+echo2 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with echo2.  If not, see <https://www.gnu.org/licenses/>.
+
 #include <iostream>
 #include <string>
 #include <regex>
@@ -9,8 +24,7 @@ using namespace std;
 
 int parse_options(int argc, char* argv[])
 {
-	//cout << "Hello Template!" << endl;
+	// echo !
 	cout << argv[1] << endl;
-
 	return 0;
 }
