9f464245a76d5a9826ba73ec97be2419ef63d657
Brian K Preston <brian8181@gmail.com>
2023-03-08 07:51:33 -0600

    add string_view test & fmtlib exmaple

---
+++ src/fmtlib.ex1.cpp
@@ -0,0 +1,26 @@
+#include <type_traits>
+#include <fmt/format.h>
+
+struct A {
+  virtual ~A() {}
+  virtual std::string name() const { return "A"; }
+};
+
+struct B : A {
+  virtual std::string name() const { return "B"; }
+};
+
+template <typename T>
+struct fmt::formatter<T, std::enable_if_t<std::is_base_of<A, T>::value, char>> :
+    fmt::formatter<std::string> {
+  template <typename FormatCtx>
+  auto format(const A& a, FormatCtx& ctx) const {
+    return fmt::formatter<std::string>::format(a.name(), ctx);
+  }
+};
+
+int main() {
+  B b;
+  A& a = b;
+  fmt::print("{}", a); // prints "B"
+}
